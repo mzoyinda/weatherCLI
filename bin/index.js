50679afd9d2b1047bc779e3356261c57a979efd9
oyindamola <oyindamoladawodu22@gmail.com>
2024-04-25 11:07:31 +0100

    fixed date

--- bin/index.js
+++ bin/index.js
@@ -3,8 +3,8 @@
 const axios = require("axios");
 
 const url = "https://api.open-meteo.com/v1/forecast";
-const current = "temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall&temperature_unit=celsius&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime";
-
+const current =
+  "temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall&temperature_unit=celsius&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime";
 
 function getWeather(location) {
   return axios
@@ -12,17 +12,13 @@ function getWeather(location) {
       params: location,
     })
     .then(({ data }) => {
+
+      console.log(data)
       const current = data.current;
       const units = data.current_units;
-      const timezone = data.timezone;
       const latitude = data.latitude;
       const longitude = data.longitude;
-      const date = new Date(current.time * 1000).toDateString();
-      const time = new Date(current.time * 1000).toLocaleTimeString({
-		hour: 'numeric',
-		minute: '2-digit',
-		hour12: true
-	});
+      const date = new Date(current.time * 1000);
 
       const weatherData = {
         temperature2m: `${current.temperature_2m}${units.temperature_2m}`,
@@ -36,9 +32,21 @@ function getWeather(location) {
       };
 
       console.log(
-          `The weather forecast for ${location.place}, ${date} at this current time ${time} \n with longitude of ${longitude} and latitude of ${latitude} is as follows: \n`,
-          `The temperature is: ${weatherData.temperature2m},\n the relative humidity is: ${weatherData.relativeHumidity2m},\n the apparent temperature is: ${weatherData.apparentTemperature},\n it is ${weatherData.isDay ? 'daytime' : 'nighttime'},\n the precipitation is: ${weatherData.precipitation},\n the rain: ${weatherData.rain},\n the showers: ${weatherData.showers},\n and the snowfall: ${weatherData.snowfall}`
-          );
+        `The weather forecast for ${location.place}, on ${date} \n with longitude of ${longitude} and latitude of ${latitude} is as follows: \n`,
+        `The temperature is: ${
+          weatherData.temperature2m
+        },\n the relative humidity is: ${
+          weatherData.relativeHumidity2m
+        },\n the apparent temperature is: ${
+          weatherData.apparentTemperature
+        },\n it is ${
+          weatherData.isDay ? "daytime" : "nighttime"
+        },\n the precipitation is: ${weatherData.precipitation},\n the rain: ${
+          weatherData.rain
+        },\n the showers: ${weatherData.showers},\n and the snowfall: ${
+          weatherData.snowfall
+        }`
+      );
     });
 }
 
